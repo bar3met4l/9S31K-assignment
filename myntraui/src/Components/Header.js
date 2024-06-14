3e3e3f3e59bc78a438e6b2a6c6396b0862241ae5
bar3met4l <aum.12109380@lpu.in>
2024-06-15 00:35:35 +0530

    myntraui

---
+++ myntraui/src/Components/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+    return(
+        <header>
+            <nav className="left-nav">
+                <img></img>
+            </nav>
+            <nav className="middle-nav"></nav>
+            <nav className="right-nav"></nav>
+        </header>
+    );
+}
+
+export default Header;  

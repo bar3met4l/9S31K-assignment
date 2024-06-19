a9ac499b13104fc46f4dff5e411e4f58ea6a6545
bar3met4l <aum.12109380@lpu.in>
2024-06-19 20:09:00 +0530

    movie-app_first_commit

---
+++ movie-app/src/App/index.js
@@ -0,0 +1,12 @@
+import styles from './app.module.scss';
+import react from 'react';
+
+function App() {
+  return (
+    <main className={styles.main}>
+        <h1>this is a free Movie App</h1>
+    </main>
+  );
+}
+
+export default App;

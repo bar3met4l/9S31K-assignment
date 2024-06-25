d5d05b2387794418207fa95e9c0c624bd86b505f
bar3met4l <aum.12109380@lpu.in>
2024-06-25 23:06:32 +0530

    button component added

--- movie-app/src/layouts/Main.js
+++ movie-app/src/layouts/Main.js
@@ -2,11 +2,13 @@ import React, { Suspense } from "react";
 import NavBar from "../components/shared/navbar";
 import { Outlet } from "react-router-dom";
 
+import styles from './layout.module.scss';
+
 function Main(){
     return(
         <Suspense fallback={<h1>Loading...</h1>}>
             <NavBar/>
-            <section>
+            <section className={styles.main}>
                  <Outlet />
             </section>
         </Suspense>
--- movie-app/src/pages/home/sections/hero/index.js
+++ movie-app/src/pages/home/sections/hero/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
+import * as assets from "../../../../assets"
 import styles from './hero.module.scss';
+import Button from '../../../../components/atoms/button';
+import { Icon } from '@iconify/react/dist/iconify.js';
+
+
 
 function Hero() {
   return (
-    <section>
-        <h1>Hero section</h1>
+    <section className={styles.container}>
+        <img src={assets.Avatar} alt='avatar-home'/>
+        <article>
+          <div className={styles.btns}>
+        <Button name={"Watch now"} handler={()=>console.log("clicked watch")} className={styles.now} >
+          <Icon icon="icon-park-solid:play"/>
+        </Button>
+        <Button name={"Watch Later"} handler={()=>console.log("clicked watch")} className={styles.later} >
+          <Icon icon="mdi:clock"/>
+        </Button>
+        </div>
+        </article>
     </section>
   );
 ;}
